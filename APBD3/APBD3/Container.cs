1d77c879e2cf04dd2d1a774ab3df1e0a789d9b71
unknown <s31302@pjwstk.edu.pl>
2025-03-23 21:01:58 +0100

    adding class Container + OverfillException

---
+++ APBD3/APBD3/Container.cs
@@ -0,0 +1,44 @@
+﻿namespace APBD3;
+
+public abstract class Container
+{
+    public static int id = 0;
+    public double mass;
+    public double height;
+    public double depth;
+    public double ownWeight;
+    public string serialNumber;
+    public double maxCapacity; //maksymalna ladownosc
+
+    protected Container(double height, double depth, double ownWeight, double maxCapacity, string type)
+    {
+        this.height = height;
+        this.depth = depth;
+        this.ownWeight = ownWeight;
+        this.maxCapacity = maxCapacity;
+        mass = 0.0;
+        serialNumber = $"KON-{type}-{++id}";
+    }
+
+    public virtual void Unloading()
+    {
+        mass = 0.0;
+    }
+
+    public virtual void Loading(double loadMass)
+    {
+        if (loadMass > maxCapacity)
+        {
+            var przekroczenie = loadMass - maxCapacity;
+            throw new OverfillException($"Nie można zaladowac!\nLadunek ktory chcesz zaladowac przekracza maksymalana ladownosc kontenera o {przekroczenie}kg." );
+        }
+        mass += loadMass;
+    }
+    
+    
+}
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message) { }
+}
